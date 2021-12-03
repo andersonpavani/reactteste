9a83883f932d5cd89d4262fd3586654c2750344a
Anderson Pavani <anderson.pavani@neainformatica.com.br>
2021-12-03 17:29:48 -0400

    Adição inicial do react e alteração simplista da página principal

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Primeiro React App feito por Anderson Pavani
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
